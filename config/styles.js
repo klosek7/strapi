c66f7e78d548598aebf5c62a9c4526617de502db
Łukasz Kłosek <lklosek@Vinderen.local>
2022-07-28 11:00:40 +0200

    add plugins

---
+++ config/styles.js
@@ -0,0 +1,9 @@
+const styles = () =>`
+  .ck.ck-editor__main .ck-blurred{
+    max-height: 200px;
+  }
+  .ck.ck-editor__main .ck-focused{
+    max-height: 500px;
+  }        
+`
+module.exports = styles;
